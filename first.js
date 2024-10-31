4458e3a06cd5648aef90d3d7a1f02ba1a6ac50cf
Priyanshu-Rana-coder <ranapriyanshu741@gmail.com>
2024-10-31 14:12:39 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,10 @@
+let AlphaSIgmaAndrewTate =document.getElementById("h")
+let count=0
+function increment(){
+    count+=1
+    AlphaSIgmaAndrewTate.innerText=count
+}
+function decrement(){
+    count-=1
+    AlphaSIgmaAndrewTate.innerText=count
+}
